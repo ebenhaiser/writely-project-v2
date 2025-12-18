f53cb149c1d2da3f3a2409dd0697a49b450b0c9e
Ebenhaiser Jonathan Caprisiano <ebenhaiser.jonathan@corp.winning-soft.com>
2025-12-18 11:21:33 +0700

    [add] reinitiate profile setting page

---
+++ app/Livewire/Profile/Setting/ProfilePicture.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Profile\Setting;
+
+use Livewire\Component;
+
+class ProfilePicture extends Component
+{
+    public function render()
+    {
+        return view('livewire.profile.setting.profile-picture');
+    }
+}
