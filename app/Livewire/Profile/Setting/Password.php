692734e43ce201a147e61aa3d83d70b43b582b71
Ebenhaiser Caprisiano <ebenhaiser@gmail.com>
2026-02-04 08:57:07 +0700

    change password setting added

--- app/Livewire/Profile/Setting/Password.php
+++ app/Livewire/Profile/Setting/Password.php
@@ -2,12 +2,50 @@
 
 namespace App\Livewire\Profile\Setting;
 
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
+use Illuminate\Support\Facades\Hash;
 
 class Password extends Component
 {
+    #[Validate('required')]
+    public $old_password;
+    #[Validate('required')]
+    public $new_password;
+    #[Validate('required')]
+    public $confirm_new_password;
+
     public function render()
     {
         return view('livewire.profile.setting.password');
+    }
+
+    public function submit()
+    {
+        if($this->new_password !== $this->confirm_new_password){
+            $this->addError('confirm_new_password', 'New password does not match.');
+            return;
+        }
+
+        $this->validate();
+
+        $user = Auth::user();
+
+        // Cek password lama
+        if (!Hash::check($this->old_password, $user->password)) {
+            $this->addError('old_password', 'Old password is incorrect.');
+            return;
+        }
+
+        // Update password baru
+        $user->password = Hash::make($this->new_password);
+        $user->save();
+
+        // Reset input fields
+        $this->reset(['old_password', 'new_password', 'confirm_new_password']);
+
+        // Bisa kirim flash message atau event
+        session()->flash('successAlert', 'Password has been updated successfully!');
     }
 }
--- resources/views/livewire/profile/setting/password.blade.php
+++ resources/views/livewire/profile/setting/password.blade.php
@@ -1,3 +1,47 @@
 <div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+    <x-layout.alert />
+    <form wire:submit.prevent="submit">
+        <div class="mb-3">
+            <label for="" class="form-label">Old Password</label>
+            <input type="password" class="form-control" wire:model.defer="old_password">
+            @if ($errors->has('old_password'))
+                <div class="form-text text-danger">
+                    {{ $errors->first('old_password') }}
+                </div>
+            @endif
+
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">New Password</label>
+            <input type="password" class="form-control" aria-describedby="" maxlength="50"
+                wire:model.defer="new_password">
+            @if ($errors->has('new_password'))
+                <div id="defaultFormControlHelp" class="form-text text-danger">
+                    {{ $errors->first('new_password') }}
+                </div>
+            @endif
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Confirm New Password</label>
+            <input type="password" class="form-control" aria-describedby="" maxlength="50"
+                wire:model.defer="confirm_new_password">
+            @if ($errors->has('confirm_new_password'))
+                <div id="defaultFormControlHelp" class="form-text text-danger">
+                    {{ $errors->first('confirm_new_password') }}
+                </div>
+            @endif
+        </div>
+        <div align="right">
+            <button type="submit" class="btn btn-primary" wire:loading.attr="disabled" wire:loading.class="opacity-50"
+                wire:target="submit">
+                <span wire:loading.remove wire:target="submit">
+                    Change Password
+                </span>
+
+                <span wire:loading wire:target="submit">
+                    <span class="spinner-border spinner-border-sm me-1" role="status"></span>
+                </span>
+            </button>
+        </div>
+    </form>
 </div>
