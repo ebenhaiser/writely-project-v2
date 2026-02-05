c2ff68d54e74dc039d7e9f9e328c26d0cf05827d
Ebenhaiser Caprisiano <ebenhaiser@gmail.com>
2026-02-05 14:57:12 +0700

    edit postcard large

--- app/Livewire/Card/PostLarge.php
+++ app/Livewire/Card/PostLarge.php
@@ -3,9 +3,52 @@
 namespace App\Livewire\Card;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Storage;
 
 class PostLarge extends Component
 {
+    public $postSlug,
+        $title,
+        $categorySlug,
+        $categoryName,
+        $content,
+        $created_at,
+        $likes,
+        $comments,
+        $authorName,
+        $authorUsername,
+        $authorProfilePictureUrl,
+        $thumbnailUrl;
+
+    public function mount($post)
+    {
+        $this->postSlug = $post->slug;
+        $this->title = $post->title;
+        $this->content = $post->content;
+        $this->created_at = $post->created_at;
+
+        $category = $post->category;
+        $this->categorySlug = $category->slug;
+        $this->categoryName = $category->name;
+        $this->likes = $post->likes;
+        $this->comments = $post->comments;
+
+        $author = $post->user;
+        $this->authorName = $author->name;
+        $this->authorUsername = $author->username;
+
+        if ($author->profile_picture && Storage::disk('public')->exists($author->profile_picture)) {
+            $this->authorProfilePictureUrl = Storage::url($author->profile_picture);
+        } else {
+            $this->authorProfilePictureUrl = asset('img/default_profile_picture.jpg');
+        }
+
+        if ($post->thumbnail && Storage::disk('public')->exists($post->thumbnail)) {
+            $this->thumbnailUrl = Storage::url($post->thumbnail);
+        } else {
+            $this->thumbnailUrl = asset('https://placehold.co/600x400');
+        }
+    }
     public function render()
     {
         return view('livewire.card.post-large');
--- resources/views/livewire/card/post-large.blade.php
+++ resources/views/livewire/card/post-large.blade.php
@@ -12,44 +12,37 @@
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-6">
-                        @php
-                            $thumbnailPath = public_path('img/postThumbnail/' . $post->thumbnail);
-                            $thumbnailUrl =
-                                $post->thumbnail && file_exists($thumbnailPath)
-                                    ? asset('img/postThumbnail/' . $post->thumbnail)
-                                    : 'https://placehold.co/600x400';
-                        @endphp
                         <img src="{{ $thumbnailUrl }}" class="card-img-top" alt="Thumbnail"
                             style="object-fit: cover; height:220px; border-radius: 0.5em">
                     </div>
                     <div class="col-md-6">
                         <div class="row mb-2 mt-2">
                             <div class="col-sm-8">
-                                <h2 class="card-title">{{ $post->title }}</h2>
+                                <h2 class="card-title">{{ $title }}</h2>
                             </div>
                             <d class="col-sm-4" align="right">
-                                <a href="{{ route('category', ['category_slug' => $post->category->slug]) }}"
+                                <a href="{{ route('category', ['category_slug' => $categorySlug]) }}"
                                     class="badge text-bg-info" style="color: white">
-                                    {{ $post->category->name }}
+                                    {{ $categoryName }}
                                 </a>
                             </d>
                         </div>
                         <p class="card-text">
-                            {{ Str::limit($post->content, 250, '...') }}
+                            {{ Str::limit($content, 250, '...') }}
                         </p>
                         {{-- <a href="#" class="card-link">Card link</a> --}}
                         <div class="d-flex gap-2">
                             <span>
-                                <i align="right">{{ $post->created_at->diffForHumans() }}</i>
+                                <i align="right">{{ $created_at->diffForHumans() }}</i>
                             </span>
                             <span>
                                 &#8226;
                             </span>
                             <span>
-                                <i class='ti ti-thumb-up'></i>{{ ' ' . count($post->likes) }}
+                                <i class='ti ti-thumb-up'></i>{{ ' ' . count($likes) }}
                             </span>
                             <span>
-                                <i class='ti ti-message-circle'></i>{{ ' ' . count($post->comments) }}
+                                <i class='ti ti-message-circle'></i>{{ ' ' . count($comments) }}
                             </span>
                         </div>
                         {{-- <button class="btn btn-outline-primary mt-2 like-btn" data-post-id="{{ $post->id }}">
@@ -63,34 +56,24 @@
             </div>
             <div class="card-footer row">
                 <div class="col-sm-10">
-                    <a href="{{ route('profile.show', ['username' => $post->user->username]) }}" class="d-flex">
+                    <a href="{{ route('profile.show', ['username' => $authorUsername]) }}" class="d-flex">
                         <span>
                             <div
                                 class="profile me-2 position-relative d-flex justify-content-end align-items-end mt-n10">
-                                @php
-                                    if (
-                                        $post->user->profile_picture &&
-                                        Storage::disk('public')->exists($post->user->profile_picture)
-                                    ) {
-                                        $card_PostLargeAvatarlUrl = Storage::url($post->user->profile_picture);
-                                    } else {
-                                        $card_PostLargeAvatarlUrl = asset('img/default_profile_picture.jpg');
-                                    }
-                                @endphp
-                                <img src="{{ $card_PostLargeAvatarlUrl }}" alt=""
+                                <img src="{{ $authorProfilePictureUrl }}" alt=""
                                     class="rounded-circle border-4 border-white-color-40">
                             </div>
                         </span>
                         <span class="my-auto ms-1">
-                            <h5 class="mt-0 mb-0">{{ $post->user->name }}</h5>
+                            <h5 class="mt-0 mb-0">{{ $authorName }}</h5>
                             <p class="mb-0 mt-0 text-body" style="text-decoration: none">
-                                {{ '@' . $post->user->username }}
+                                {{ '@' . $authorUsername }}
                             </p>
                         </span>
                     </a>
                 </div>
                 <div class="col-sm-2 my-auto" align="right">
-                    <a href="{{ route('post.show', ['slug' => $post->slug]) }}" class="btn btn-outline-secondary">
+                    <a href="{{ route('post.show', ['slug' => $postSlug]) }}" class="btn btn-outline-secondary">
                         Read more &rarr;
                     </a>
                 </div>
