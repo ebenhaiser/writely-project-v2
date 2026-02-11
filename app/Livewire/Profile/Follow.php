812805b85a76e516b4f806ba3b7efe0f8dc2d9bc
Ebenhaiser Jonathan Caprisiano <ebenhaiser.jonathan@corp.winning-soft.com>
2026-02-11 16:37:06 +0700

    follow page done

--- app/Livewire/Profile/Follow.php
+++ app/Livewire/Profile/Follow.php
@@ -3,13 +3,19 @@
 namespace App\Livewire\Profile;
 
 use App\Models\User;
+use Livewire\WithPagination;
 use Livewire\Component;
 
 class Follow extends Component
 {
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+
     public $profile;
     public $name;
     public $follow; // 'follower' or 'following'
+    public $title;
+    public $keyword;
 
     public function mount($username, $follow)
     {
@@ -17,8 +23,33 @@ class Follow extends Component
         $this->name = $this->profile->name;
         $this->follow = $follow;
     }
+
     public function render()
     {
-        return view('livewire.page.follow');
+        if ($this->follow === 'following') {
+            $this->title = $this->profile->name . ' (@' . $this->profile->username . ') Following';
+
+            $usersQuery = $this->profile->following();
+        } elseif ($this->follow === 'follower') {
+            $this->title = $this->profile->name . ' (@' . $this->profile->username . ') Followers';
+
+            $usersQuery = $this->profile->followers();
+        }
+
+        if ($this->keyword) {
+            $usersQuery->where(function ($q) {
+                $q->where('users.name', 'like', '%' . $this->keyword . '%')
+                    ->orWhere('users.username', 'like', '%' . $this->keyword . '%');
+            });
+        }
+
+        $users = $usersQuery->paginate(12);
+
+        return view('livewire.page.follow', compact('users'));
+    }
+
+    public function updatedKeyword()
+    {
+        $this->resetPage();
     }
 }
--- resources/views/livewire/page/follow.blade.php
+++ resources/views/livewire/page/follow.blade.php
@@ -1,13 +1,28 @@
 <div>
     <div class="card">
         <div class="card-header">
-            <h1>{{ $name }} {{ $follow == 'following' ? 'Following' : 'Followers' }}</h1>
+            <h1>{{ $title }}</h1>
         </div>
         <div class="card-body">
             <div>
                 <input type="text" class="form-control w-100" aria-describedby="" maxlength="50"
-                    placeholder="Search {{ $follow == 'following' ? 'Following' : 'Followers' }} ..." wire:model.defer="">
+                    placeholder="Search {{ $follow == 'following' ? 'Following' : 'Followers' }} ..." wire:model.live="keyword">
             </div>
         </div>
     </div>
+
+    @if (count($users) > 0)
+        <div class="row">
+            @foreach ($users as $user)
+                <div class="col-md-4">
+                    <x-cards.user :user="$user" />
+                </div>
+            @endforeach
+        </div>
+        {{ $users->links() }}
+    @else
+        <div align="center">
+            <i>No data found.</i>
+        </div>
+    @endif
 </div>
